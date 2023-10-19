ea6b85c5902b91e8ea9841f300cd9b9d61cac9a8
Clarissa Au <auyeungyat@yahoo.com.hk>
2023-10-20 02:00:13 +0800

    allowed customizable call to newtonSqrt to precs

--- pyDataStructAlgo/algorithms/numerical/sqrt.py
+++ pyDataStructAlgo/algorithms/numerical/sqrt.py
@@ -1,9 +1,9 @@
-def newtonSqrt(val: float) -> float:
+def newtonSqrt(val: float, prec:int=12) -> float:
 
     if val < 0:
         raise ValueError("newtonSqrt input must be nonnegative.")
 
-    levelOfTolerance = 10**(-12)
+    levelOfTolerance = 10**(-prec)
     sqrt = val
 
     while (abs(val - sqrt * sqrt)) > levelOfTolerance:
