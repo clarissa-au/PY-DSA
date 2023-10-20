e948de684caf488615411a09658379d6a9dddf6b
Clarissa Au <auyeungyat@yahoo.com.hk>
2023-10-20 18:13:23 +0800

    cleaned fastExp

--- pyDataStructAlgo/algorithms/recursion/fastExponentiation.py
+++ pyDataStructAlgo/algorithms/recursion/fastExponentiation.py
@@ -1,9 +1,8 @@
 def fastExp(base: float, exponent:int) -> float:
     if(exponent == 0):
         return 1
-    elif(exponent < 0):
+    if(exponent < 0):
         return fastExp(1/base, -exponent)
-    elif(exponent%2 == 0):
+    if(exponent%2 == 0):
         return fastExp(base**2, exponent >> 1)
-    else:
-        return base * fastExp(base**2, exponent >> 1)
+    return base * fastExp(base**2, exponent >> 1)
